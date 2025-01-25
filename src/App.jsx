6d40028463ff4095177cfb282b5fae8e0b94d14a
Torandori <vaviktova@gmail.com>
2025-01-25 22:41:32 +0100

    Initiated React app, created Test component

--- src/App.jsx
+++ src/App.jsx
@@ -1,36 +1,9 @@
-// import './assets/scss/style.scss'
-// import 'react-toastify/dist/ReactToastify.css';
+import React from 'react';
+import Test from './components/Test.jsx'
 
-// const Wrapper = ({children}) => {
-//   const location = useLocation();
-//   useLayoutEffect(() => {
-//     document.documentElement.scrollTo(0, 0);
-//   }, [location.pathname]);
-//   return children
-// }  
 function App() {
   return (
-    // <Wrapper>
-      // <ParallaxProvider>
-        // <Routes>
-          // <Route path="/" element={<Default />}>
-          //   <Route index element={<Home />}/>
-          //   <Route path="/case" element={<Case />}/> 
-          //   <Route path="/services" element={<Services />}/>
-          //   <Route path="/services/:slug" element={<ServiceDetails />}/>
-          //   <Route path="/about" element={<About />}/>
-          //   <Route path="/news" element={<News />}/>
-          //   <Route path="news/:hash" element={<NewsDetails/>}/>
-          //   {/* <Route path="news/:hash" element={<NewsDetailsFetch />}/> */}
-          // </Route>
-          // <Route path="*" element={<NotFound />}/>
-          // <Route path="/" element={<DarkLayout />}>
-          //   <Route path="/contacts" element={<Contacts />}/>
-          // </Route>
-        // </Routes>
-      // </ParallaxProvider>
-      // <ToastContainer theme="dark" pauseOnHover position="bottom-left"/>
-    // </Wrapper>
+    <Test/>
   )
 }
 
--- src/main.jsx
+++ src/main.jsx
@@ -1,17 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-// import { BrowserRouter } from 'react-router-dom'
 import App from './App.jsx'
-import './assets/scss/style.scss'
-import { HashRouter } from 'react-router-dom'
-import { HelmetProvider } from 'react-helmet-async';
+// import './assets/scss/style.scss'
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  // <React.StrictMod?e>
-  //   <HashRoute?r>
-  //     <HelmetPr?ovider>
-        <App />
-  //     </HelmetProvider>
-  //   </HashRouter>
-  // </React.StrictMode>,
+  <App />
 )
+
+
